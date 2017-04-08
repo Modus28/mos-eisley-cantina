2902aef2c148d78cb82983ee52b92957a9154a43
LK <lxk201@case.edu>
2017-04-08 13:47:41 -0400

    added sql script to insert employee

---
+++ sqlTables/insertIntoEmployee.sql
@@ -0,0 +1,10 @@
+insert into employee(e_id, ename, salary, datehired) values (00000, "Hinata", 2000, "12/15");
+insert into employee(e_id, ename, salary, datehired) values (00001, "Kotori", 5000, "1/1");
+insert into employee(e_id, ename, salary, datehired) values (00010, "Umi", 7500,"1/1");
+insert into employee(e_id, ename, salary, datehired) values(00011, "Sakura", 3000, "6/3");
+insert into employee(e_id, ename, salary, datehired) values(00100, "Ruby", 1500, "7/30");
+insert into employee(e_id, ename, salary, datehired) values(00101, "Honoka", 2500, "9/10");
+insert into employee(e_id, ename, salary, datehired) values(00110, "Aoi", 3600,"4/2");
+insert into employee(e_id, ename, salary, datehired) values(00111, "Rin", 3100, "9/10");
+insert into employee(e_id, ename, salary, datehired) values(01000, "Kekyo", 1000, "10/30");
+insert into employee(e_id, ename, salary, datehired) values(01001, "Kagome", 4100, "8/25");
