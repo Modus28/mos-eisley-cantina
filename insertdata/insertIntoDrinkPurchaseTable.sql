f3ff68fd6125b338c285f7c10e29c45dfd8b6cb0
Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>
2017-04-12 17:39:24 -0400

    added into drinkpurchase table, will be adding more into the table tonight
    Signed-off-by: Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>

---
+++ insertdata/insertIntoDrinkPurchaseTable.sql
@@ -0,0 +1,11 @@
+insert into drinkpurchase(billNum, d_id, qty) values (0, 18, 2);
+insert into drinkpurchase(billNum, d_id, qty) values (0, 3, 1);
+insert into drinkpurchase(billNum, d_id, qty) values (1, 4, 2);
+insert into drinkpurchase(billNum, d_id, qty) values (1, 16, 1);
+insert into drinkpurchase(billNum, d_id, qty) values (1, 11, 2);
+insert into drinkpurchase(billNum, d_id, qty) values (1, 18, 2);
+insert into drinkpurchase(billNum, d_id, qty) values (10, 17, 1);
+insert into drinkpurchase(billNum, d_id, qty) values (10, 9, 2);
+insert into drinkpurchase(billNum, d_id, qty) values (10, 18, 3);
+insert into drinkpurchase(billNum, d_id, qty) values (11, 19, 1);
+insert into drinkpurchase(billNum, d_id, qty) values (11, 2, 1);
