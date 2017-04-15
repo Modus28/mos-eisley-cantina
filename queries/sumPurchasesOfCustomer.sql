19414df585f768f11276d67167e2477b7ca719bf
Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>
2017-04-15 17:39:05 -0400

    added some basic sql queries for summing up total bill of customer
    Signed-off-by: Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>

---
+++ queries/sumPurchasesOfCustomer.sql
@@ -0,0 +1,37 @@
+
+select bill.billNum, sum(priceTable.price * foodpurchase.qty)
+from foodpurchase, bill, fooditem, priceTable
+where foodpurchase.billNum = bill.billNum
+and bill.c_id = 00000
+and fooditem.f_id = foodpurchase.f_id
+and fooditem.p_id = priceTable.p_id
+group by(bill.billNum)
+
+
+select bill.billNum, sum(priceTable.price * drinkpurchase.qty)
+from drinkpurchase, bill, drinks, priceTable
+where drinkpurchase.billNum = bill.billNum
+and bill.c_id = 00000
+and drinks.d_id = drinkpurchase.d_id
+and drinks.p_id = priceTable.p_id
+group by(bill.billNum)
+
+
+Delimiter \\
+CREATE PROCEDURE sumfoodpurchaseofcustomer(c_id integer(5))
+BEGIN
+SET @q1 =CONCAT("select bill.billNum, sum(priceTable.price * foodpurchase.qty)
+from foodpurchase, bill, fooditem, priceTable
+where foodpurchase.billNum = bill.billNum
+and bill.c_id =", c_id," and fooditem.f_id = foodpurchase.f_id
+and fooditem.p_id = priceTable.p_id
+group by(bill.billNum)");
+Prepare stmt1 From @q1;
+execute stmt1;
+deallocate prepare stmt1;
+End\\
+
+call sumfoodpurchaseofcustomer(00000);
+
+
+
