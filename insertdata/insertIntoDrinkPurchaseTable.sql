c73ebb30b04dc4340dae022f9398abe0b5f64d47
Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>
2017-04-15 16:32:28 -0400

    added more inserts into the drink table
    Signed-off-by: Dina Benayad-Cherif <dina.benayad.cherif@gmail.com>

--- insertdata/insertIntoDrinkPurchaseTable.sql
+++ insertdata/insertIntoDrinkPurchaseTable.sql
@@ -9,3 +9,84 @@ insert into drinkpurchase(billNum, d_id, qty) values (10, 9, 2);
 insert into drinkpurchase(billNum, d_id, qty) values (10, 18, 3);
 insert into drinkpurchase(billNum, d_id, qty) values (11, 19, 1);
 insert into drinkpurchase(billNum, d_id, qty) values (11, 2, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (100, 5, 1);
+insert into drinkpurchase(billNum, d_id, qty) values (100, 6, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (101, 20, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (110, 3, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (111, 8, 1);
+insert into drinkpurchase(billNum, d_id, qty) values (111, 18, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (1000, 19, 1);
+insert into drinkpurchase(billNum, d_id, qty) values (1000, 4, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (1001, 18, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (1010, 7, 2);
+
+insert into drinkpurchase(billNum, d_id, qty) values (1011, 1, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (1100, 6, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (1101, 7, 1);
+insert into drinkpurchase(billNum, d_id, qty) values (1101, 18, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (1110, 18, 1);
+
+insert into drinkpurchase(billNum, d_id, qty) values (1111, 4, 1);
+insert into drinkpurchase(billNum, d_id, qty) values (1111, 5, 1);
+
+/*
+insert into foodpurchase(billNum, f_id, qty) values (10000, 1, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10000, 4, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10000, 8, 1);
+
+insert into foodpurchase(billNum, f_id, qty) values (10001, 8, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10001, 7, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10001, 5, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10001, 11, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10001, 1, 1);
+
+insert into foodpurchase(billNum, f_id, qty) values (10010, 2, 2);
+insert into foodpurchase(billNum, f_id, qty) values (10010, 3, 2);
+
+insert into foodpurchase(billNum, f_id, qty) values (10011, 1, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10011, 2, 2);
+insert into foodpurchase(billNum, f_id, qty) values (10011, 3, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10011, 4, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10011, 5, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10011, 6, 1);
+
+insert into foodpurchase(billNum, f_id, qty) values (10100, 1, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10100, 2, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10100, 6, 1);
+insert into foodpurchase(billNum, f_id, qty) values (10100, 11, 1);
+
+insert into foodpurchase(billNum, f_id, qty) values (10101, 11, 2);
+
+insert into foodpurchase(billNum, f_id, qty) values (10110, 2, 2);
+insert into foodpurchase(billNum, f_id, qty) values (10110, 1, 1);
+
+insert into foodpurchase(billNum, f_id, qty) values (10111, 1, 1);
+
+insert into foodpurchase(billNum, f_id, qty) values (11000, 4, 2);
+insert into foodpurchase(billNum, f_id, qty) values (11000, 3, 1);
+
+insert into foodpurchase(billNum, f_id, qty) values (11001, 1, 1);
+insert into foodpurchase(billNum, f_id, qty) values (11001, 2, 1);
+insert into foodpurchase(billNum, f_id, qty) values (11001, 3, 1);
+
+insert into foodpurchase(billNum, f_id, qty) values (11010, 3, 1);
+insert into foodpurchase(billNum, f_id, qty) values (11010, 7, 1);
+insert into foodpurchase(billNum, f_id, qty) values (11010, 9, 1);
+
+insert into foodpurchase(billNum, f_id, qty) values (11011, 9, 1);
+insert into foodpurchase(billNum, f_id, qty) values (11011, 10, 1);
+
+insert into foodpurchase(billNum, f_id, qty) values (11100, 11, 3);
+
+insert into foodpurchase(billNum, f_id, qty) values (11101, 10, 1);
+*/
